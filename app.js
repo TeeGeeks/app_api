540955f3258c8cd16d36fa15f17ea34ddb65d331
TeeGeeks <olabodetoheeb622@gmail.com>
2024-05-29 13:39:44 +0100

    Initialcommit

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const priceController = require("./controllers/priceController");
 const { loginWithGoogle } = require("./controllers/authController");
 
 const app = express();
-const port = process.env.port || 5000;
+const port = process.env.port || 3000;
 
 // MongoDB connection string
 const mongoURI = process.env.mongoURI;
