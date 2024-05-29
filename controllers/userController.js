b5d150bcf57e1b40219daecfd4a95e6cab0de63b
TeeGeeks <olabodetoheeb622@gmail.com>
2024-05-29 14:14:13 +0100

    commit 3

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,5 @@
 const User = require("../models/users");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const fs = require("fs");
 const { generateRandomToken } = require("../util/fnc");
